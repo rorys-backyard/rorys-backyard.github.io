e05eb8569e051457fd7cc4569a065f3a8762bbad
michaeljohnbrown3 <78980602+michaeljohnbrown3@users.noreply.github.com>
2021-08-23 22:47:08 -0400

    master

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("endgame").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+function myFunction() {
+    document.getElementById("Owl").style.width = "0%";
+  }
