2c4398d97da318ef53c1770eff3138c55da7ec42
michaeljohnbrown3 <78980602+michaeljohnbrown3@users.noreply.github.com>
2021-08-28 12:40:55 -0400

    master

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
 
-  AFRAME.registerComponent('markerhandler', {
+
+AFRAME.registerComponent('owl', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#owl");
+        const aEntity = document.querySelector('#owl');
 
-        animatedMarker.addEventListener('touch', function(ev, target){
+        animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
@@ -20,4 +21,132 @@ function closeNav() {
                 aEntity.setAttribute('scale', scale);
             }
         });
+}});
+
+AFRAME.registerComponent('raccoon', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#raccoon');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('skunk', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#skunk');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('frog', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#frog');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('bobcat', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#bobcat');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('deer', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#deer');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('mockingbird', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#mockingbird');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('snake', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#snake');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
+
+AFRAME.registerComponent('turtle', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#turtle');
+
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
 }});
