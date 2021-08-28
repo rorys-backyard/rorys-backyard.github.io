809bbc38ba6cfe09b71837295f1c4cb552ca6760
michaeljohnbrown3 <78980602+michaeljohnbrown3@users.noreply.github.com>
2021-08-28 16:49:23 -0400

    master

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function openNav() {
-    document.getElementById("endgame").style.width = "100%";
-  }
-  
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
@@ -150,3 +146,46 @@ AFRAME.registerComponent('turtle', {
           }
       });
 }});
+
+const TIME_LIMIT = 60;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("timer").innerHTML = formatTime(
+      timeLeft
+    );
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function openNav() {
+  document.getElementById("endgame").style.width = "100%";
+}
