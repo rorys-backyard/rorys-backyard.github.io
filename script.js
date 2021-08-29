a86c3d7e77ac2f72f5bf1b7c1fa5c58b2704752c
michaeljohnbrown3 <78980602+michaeljohnbrown3@users.noreply.github.com>
2021-08-29 12:57:49 -0400

    master

--- script.js
+++ script.js
@@ -1,170 +1,174 @@
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function cameraFlash() {
+  document.getElementById("cameraflash").style.visibility = visible;
+}
 
 let score = 0;
 
 AFRAME.registerComponent('owl', {
 
-    init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector('#owl');
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector('#owl');
 
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = 0);
-                aEntity.setAttribute('scale', scale);
-                score = score += 1;
-                document.getElementById('score').innerHTML = "Animals Found: " +score;
-            }
-        });
-        
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = 0);
+              aEntity.setAttribute('scale', scale);
+              score = score += 1;
+              document.getElementById('score').innerHTML = "Animals Found: " +score;
+          }
+      });
+      
 }});
 
 AFRAME.registerComponent('raccoon', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#raccoon');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#raccoon');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('skunk', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#skunk');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#skunk');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('frog', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#frog');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#frog');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('bobcat', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#bobcat');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#bobcat');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('deer', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#deer');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#deer');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('mockingbird', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#mockingbird');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#mockingbird');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('snake', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#snake');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#snake');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 AFRAME.registerComponent('turtle', {
 
-  init: function() {
-      const animatedMarker = document.querySelector("#animated-marker");
-      const aEntity = document.querySelector('#turtle');
+init: function() {
+    const animatedMarker = document.querySelector("#animated-marker");
+    const aEntity = document.querySelector('#turtle');
 
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = 0);
-              aEntity.setAttribute('scale', scale);
-              score = score += 1;
-              document.getElementById('score').innerHTML = "Animals Found: " +score;
-          }
-      });
+    animatedMarker.addEventListener('click', function(ev, target){
+        const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        if (aEntity && intersectedElement === aEntity) {
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = 0);
+            aEntity.setAttribute('scale', scale);
+            score = score += 1;
+            document.getElementById('score').innerHTML = "Animals Found: " +score;
+        }
+    });
 }});
 
 const TIME_LIMIT = 60;
@@ -173,40 +177,59 @@ let timeLeft = TIME_LIMIT;
 let timerInterval = null;
 
 function onTimesUp() {
-  clearInterval(timerInterval);
-  openNav();
+clearInterval(timerInterval);
+openNav();
 }
 
 function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("timer").innerHTML = formatTime(
-      timeLeft
-    );
+timerInterval = setInterval(() => {
+  timePassed = timePassed += 1;
+  timeLeft = TIME_LIMIT - timePassed;
+  document.getElementById("timer").innerHTML = formatTime(
+    timeLeft
+  );
 
-    if (timeLeft === 0 || score == 9) {
-      onTimesUp();
-    }
-  }, 1000);
+  if (timeLeft === 0 || score == 9) {
+    onTimesUp();
+  }
+}, 1000);
 }
 
 function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+const minutes = Math.floor(time / 60);
+let seconds = time % 60;
 
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
+if (seconds < 10) {
+  seconds = `0${seconds}`;
+}
 
-  return `${minutes}:${seconds}`;
+if (seconds <= 15) {
+  document.getElementById("timer").style.backgroundColor = "yellow";
+}
+
+if (seconds <= 5) {
+  document.getElementById("timer").style.backgroundColor = "red";
+}
+
+return `${minutes}:${seconds}`;
 }
 
 function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+const rawTimeFraction = timeLeft / TIME_LIMIT;
+return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 
 function openNav() {
-  document.getElementById("endgame").style.width = "100%";
+document.getElementById("endgame").style.width = "100%";
+
+if (score == 1) {
+  document.getElementById('finalscore').innerHTML = score + " ANIMAL";
+}
+else if (score == 9) {
+  document.getElementById('finalscore').innerHTML = "ALL THE ANIMALS"
+}
+else {
+  document.getElementById('finalscore').innerHTML = score + " ANIMALS";
+}
+
 }
