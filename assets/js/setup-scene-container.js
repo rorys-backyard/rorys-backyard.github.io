e250827fa20af6ebb34e5124ca160169a93252cb
michaeljohnbrown3 <78980602+michaeljohnbrown3@users.noreply.github.com>
2021-09-17 17:16:21 -0400

    michael

--- assets/js/setup-scene-container.js
+++ assets/js/setup-scene-container.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    $.getJSON('./assets/data/animals.json', (animals) => {
-        const aMarker = document.querySelector('#animated-marker');
+    fetch('./assets/data/animals.json')
+        .then(response => response.json())
+        .then(animals => {
+            const aMarker = document.querySelector('#animated-marker');
 
-        animals.forEach((animal) => {
-            const { aImageAttributes, id, name } = animal;
+            animals.forEach((animal) => {
+                const { aImageAttributes, id, name } = animal;
 
-            const aImage = document.createElement('a-image');
+                const aImage = document.createElement('a-image');
 
-            aImage.setAttribute(id, '');
-            aImage.setAttribute('id', id);
-            aImage.setAttribute('src', `./assets/images/${id}.png`);
-            aImage.setAttribute('alt', name);
+                aImage.setAttribute(id, '');
+                aImage.setAttribute('id', id);
+                aImage.setAttribute('src', `./assets/images/${id}.png`);
+                aImage.setAttribute('alt', name);
 
-            // Research: Loops for Objects
-            for (const attribute in aImageAttributes) {
-                const value = aImageAttributes[attribute];
+                // Research: Loops for Objects
+                for (const attribute in aImageAttributes) {
+                    const value = aImageAttributes[attribute];
 
-                aImage.setAttribute(attribute, value);
-            }
+                    aImage.setAttribute(attribute, value);
+                }
 
-            aMarker.appendChild(aImage);
+                aMarker.appendChild(aImage);
+            });
         });
-    });
 });
